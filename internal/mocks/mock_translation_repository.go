d476de06beaae0d9819496395f023886d1869126
grzegorzpapaj <137798629+grzegorzpapaj@users.noreply.github.com>
2025-02-27 11:34:15 +0100

    Add mock translation repository

---
+++ internal/mocks/mock_translation_repository.go
@@ -0,0 +1,22 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/grzegorzpapaj/graphql-dictionary-api/internal/graph/model"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockTranslationRepository struct {
+	mock.Mock
+}
+
+func (m *MockTranslationRepository) AddTranslation(ctx context.Context, polishWordID *string, polishWord *string, translation *model.AddTranslationInput) (*model.Translation, error) {
+
+	args := m.Called(ctx, polishWordID, polishWord, translation)
+
+	if result, ok := args.Get(0).(*model.Translation); ok {
+		return result, args.Error(1)
+	}
+	return nil, args.Error(1)
+}
